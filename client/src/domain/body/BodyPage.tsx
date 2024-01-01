4d7af7b4426a97db6d4fb902aa0f9c8179cc8b19
dustin <dustin.holm@gmx.net>
2024-01-01 21:15:00 +0100

    start client

---
+++ client/src/domain/body/BodyPage.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/joy"
+import { memo } from "react"
+
+function BodyPage() {
+  return <Box>
+
+  </Box>
+}
+
+export default memo(BodyPage)
---
+++ client/src/domain/home/HomePage.tsx
@@ -0,0 +1,7 @@
+import {  memo } from "react"
+
+function HomePage() {
+  return "hi"
+}
+
+export default memo(HomePage)
