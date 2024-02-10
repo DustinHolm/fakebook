c0cc5f4ec9e50f779b3c86363a03a1132d1024f6
Dustin <dustin.holm@gmx.net>
2024-02-10 14:02:00 +0100

    simpler config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,30 +1,19 @@
-import { PluginOption, defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { transformSync } from "@swc/core";
-import swc from "./.swcrc";
-import relay from "vite-plugin-relay";
-
-const _relay: PluginOption = {
-  name: "vite:relay",
-  transform(src, id) {
-    if (/.(t|j)sx?/.test(id) && src.includes("graphql`")) {
-      const out = transformSync(src, swc);
-
-      if (!out?.code) {
-        throw new Error(`vite-plugin-relay: Failed to transform ${id}`);
-      }
-
-      const code = out.code;
-      const map = out.map;
-
-      return {
-        code,
-        map,
-      };
-    }
-  },
-};
 
 export default defineConfig({
-  plugins: [relay, react()],
+  plugins: [
+    react({
+      plugins: [
+        [
+          "@swc/plugin-relay",
+          {
+            rootDir: ".",
+            language: "typescript",
+            eagerEsModules: true,
+          },
+        ],
+      ],
+    }),
+  ],
 });
