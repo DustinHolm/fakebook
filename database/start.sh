12270c4a280df8cbaefd6698e3f12a9c53f776c7
Dustin <dustin.holm@gmx.net>
2024-02-11 13:47:02 +0100

    update tasks

--- database/start.sh
+++ database/start.sh
@@ -4,11 +4,14 @@ docker images | grep fakebook-db > /dev/null || $(dirname -- "$0")/build.sh
 docker ps | grep fakebook-db > /dev/null && docker stop fakebook-db
 docker ps -a | grep fakebook-db > /dev/null && docker rm fakebook-db
 
-
-docker run \
-    -p 127.0.0.1:5432:5432 \
-    -v fakebook-volume:/var/lib/postgresql/data \
-    --name fakebook-db \
-    --rm \
-    fakebook-db \
-    -c log_statement=all
+(
+    trap "kill 0" SIGINT
+    docker run \
+        -it \
+        -p 127.0.0.1:5432:5432 \
+        -v fakebook-volume:/var/lib/postgresql/data \
+        --name fakebook-db \
+        --rm \
+        fakebook-db \
+        -c log_statement=all
+)
