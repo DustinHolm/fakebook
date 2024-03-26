e7323c9884768d4962cdf9f32b8ffb064e3a31ea
Dustin <dustin.holm@gmx.net>
2024-03-26 22:49:57 +0100

    more benches

---
+++ benchmark/userFriends.js
@@ -0,0 +1,44 @@
+import exec from "k6/execution";
+import encoding from "k6/encoding";
+import { check } from "k6";
+import { User, UserFriends } from "./util/requests.js";
+import { handleSummaryFn } from "./util/summary.js";
+
+export const options = {
+  thresholds: {
+    checks: [
+      {
+        threshold: "rate == 1",
+        abortOnFail: true,
+      },
+    ],
+    http_req_duration: ["max < 500", "p(95) < 330", "med < 150"],
+  },
+  scenarios: {
+    mixed_requests_spike: {
+      executor: "constant-vus",
+      vus: 1000,
+      duration: "20s",
+      exec: "mixed",
+      env: { PAGINATION: "3" },
+    },
+  },
+};
+
+const maxUserIdQuery = 5000;
+
+const mixedRequests = [User, UserFriends];
+export const mixed = () => {
+  const i = exec.scenario.iterationInInstance % mixedRequests.length;
+  let id = (exec.scenario.iterationInInstance % maxUserIdQuery) + 1;
+  id = encoding.b64encode(id + "AppUser", "url");
+
+  const res = mixedRequests[i](id);
+
+  check(res, {
+    "response did not contain error": (r) =>
+      r.status == 200 && !!r.json() && !r.json().errors,
+  });
+};
+
+export const handleSummary = (data) => handleSummaryFn(data, "userFriends");
--- benchmark/util/requests.js
+++ benchmark/util/requests.js
@@ -106,6 +106,62 @@ export const UserFriendsPostsComments = (id, nPaginated) =>
     })
   );
 
+export const UserPosts = (id, nPaginated) =>
+  http.post(
+    graphqlUrl,
+    JSON.stringify({
+      query: `query UserPosts($id: ID!, $nPaginated: Int) {
+                user(id: $id) {
+                  id
+                  firstName
+                  lastName
+                  posts(first: $nPaginated) {
+                    edges {
+                      node {
+                        content
+                      }
+                    }
+                  }
+                }
+              }`,
+      variables: {
+        id,
+        nPaginated,
+      },
+    })
+  );
+
+export const UserPostsComments = (id, nPaginated) =>
+  http.post(
+    graphqlUrl,
+    JSON.stringify({
+      query: `query UserPostsComments($id: ID!, $nPaginated: Int) {
+                user(id: $id) {
+                  id
+                  firstName
+                  lastName
+                  posts(first: $nPaginated) {
+                    edges {
+                      node {
+                        comments(first: $nPaginated) {
+                          edges {
+                            node {
+                              content
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }`,
+      variables: {
+        id,
+        nPaginated,
+      },
+    })
+  );
+
 export const UserThriceNestedFriends = (id) =>
   http.post(
     graphqlUrl,
